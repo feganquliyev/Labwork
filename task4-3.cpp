6bc834a03fbc7278453838450b4063054464fac2
feganquliyev <feganquliyev8@gmail.com>
2025-11-02 06:15:21 -0800

    Added task4-3
    Level 4 - Task 3

---
+++ task4-3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    for (int i = 2; i <= N; i++) {
+        bool prime = true;
+        for (int j = 2; j <= sqrt(i); j++) {
+            if (i % j == 0) {
+                prime = false;
+                break;
+            }
+        }
+        if (prime) {
+            cout << i << " ";
+        }
+    }
+    return 0;
+}
