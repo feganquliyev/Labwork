96e2fd301aca8a4e4700c351e5664d59ad85209c
feganquliyev <feganquliyev8@gmail.com>
2025-11-02 00:16:42 -0700

    Added task 2-4
    Level 2 - Task 4

---
+++ task 2-4.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+#include <cmath>
+int main() {
+    int a;
+    cin >> a;
+    if (a % 2 == 0){
+        cout << "Cutdur";
+    }
+    else{
+        cout << "Tekdir";
+    }
+    return 0;
+    
+
+}
