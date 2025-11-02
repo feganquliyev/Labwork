a34df66bd66187fd9394b139489999f961241eda
feganquliyev <feganquliyev8@gmail.com>
2025-11-01 22:54:15 -0700

    Added Task 6
    Level 1 - Task 6

---
+++ Task 1-6.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+#include <cmath>
+int main() {
+    int a;
+    int b;
+    int c;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    int eo = (a + b + c) / 3.0;
+    cout << eo;
+   return 0;
+}
