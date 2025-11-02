056031e39670e3d747f86b01a627ddddc8a2b2c9
feganquliyev <feganquliyev8@gmail.com>
2025-11-01 22:58:44 -0700

    Added Task 9
    Level 1 - Task 9

---
+++ Task 1-9.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+using namespace std;
+#include <cmath>
+int main() {
+    int a;
+    cin >> a;
+    if (a > 0){
+        cout << "Eded musbetdir";
+    }
+    else if(a < 0){
+        cout << "Eded menfidir";
+        
+    }
+    else{
+        cout << "Sifirdir";
+    }
+    
+    return 0;
+    
+
+}
