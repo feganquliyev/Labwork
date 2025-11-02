71befced052e51ca6405eb7d7b05e4d283fd2aad
feganquliyev <feganquliyev8@gmail.com>
2025-11-01 22:52:45 -0700

    Added Task 5
    Level 1 - Task 5

---
+++ Task 1-5.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+using namespace std;
+#include <cmath>
+int main() {
+    int selsi;
+    cin >> selsi;
+    int farenheyt = selsi * (9.0 / 5.0) + 32;
+    cout << farenheyt;
+    return 0;
+}
