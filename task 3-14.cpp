ba6e4ebe60cced328acec5ee6fe1e061cce1fcb0
feganquliyev <feganquliyev8@gmail.com>
2025-11-02 02:06:16 -0800

    Added task 3-14
    Level 3 - Task 14

---
+++ task 3-14.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+#include <cmath>
+int main(){
+    for(int i = 1; i <= 100; i++){
+        if(i % 3 == 0 & i % 7 == 0){
+            cout << i << endl;
+        }
+    }
+    return 0;
+}
