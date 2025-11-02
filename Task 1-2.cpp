ca1b07e1f126f5833bfea68f3d4bd548cbe32b89
feganquliyev <feganquliyev8@gmail.com>
2025-11-01 22:44:56 -0700

    Added Task 2
    Level 1 - Task 2

---
+++ Task 1-2.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    int b;
+    int cem = 0;
+    int hasil = 1;
+    int bolme_1 = 1;
+    int bolme_2 = 1;
+    cin >> a;
+    cin >> b;
+    cem = a + b;
+    hasil = a * b;
+    bolme_1 = a / b;
+    bolme_2 = b / a;
+    cout << hasil << endl << bolme_1 << endl << bolme_2 << endl << cem;
+    return 0;
+}
+  
