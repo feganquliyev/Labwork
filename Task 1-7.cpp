c4b2d53434b9147ae00ca933cddd2f52ed3ad6d3
feganquliyev <feganquliyev8@gmail.com>
2025-11-01 22:55:41 -0700

    Added Task 7
    Level 1 - Task 7

---
+++ Task 1-7.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+#include <cmath>
+int main() {
+    int a;
+    cin >> a;
+    int sahe = pow(a, 2) ;
+    int perimetr = a * 4;
+    cout << perimetr << endl << sahe;
+    return 0;
+    
+
+}
